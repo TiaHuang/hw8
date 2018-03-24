cc066a04b84fee6a679e65559c73bd31453725eb
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-03-23 22:58:47 -0700

    Update doctor.ino

--- doctor.ino
+++ doctor.ino
@@ -7,12 +7,12 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  for (int fadeValue = 0 ; fadeValue <= 800; fadeValue += 300) {
+  for (int fadeValue = 0 ; fadeValue <= 1200; fadeValue += 500) {
     analogWrite(13, fadeValue);
     delay(1000);
   }
 
-  for (int fadeValue = 800 ; fadeValue >= 0; fadeValue -= 300) {
+  for (int fadeValue = 1200 ; fadeValue >= 800; fadeValue -= 500) {
     analogWrite(13, fadeValue);
     delay(1000);
   }
