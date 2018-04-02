bf70e273cb2d4b1cb3a80c991461f6952c01c566
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-04-01 22:46:53 -0700

    Update meeting.ino

--- meeting.ino
+++ meeting.ino
@@ -2,18 +2,18 @@ int sensorValue = 0;
 int sensorPin = A0;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
 
   Serial.begin(9600);
 }
 void loop() {
   for (int fadeValue = 0 ; fadeValue <= 1000; fadeValue += 500) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(1000);
   }
 
   for (int fadeValue = 1000 ; fadeValue >= 0; fadeValue -= 500) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(1000);
   }
 }
