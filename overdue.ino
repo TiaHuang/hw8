0a3e99dec8a507d1ff728647a3cb7d20619b62cb
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-04-01 22:47:28 -0700

    Update overdue.ino

--- overdue.ino
+++ overdue.ino
@@ -5,7 +5,7 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -13,7 +13,7 @@ void loop() {
   // fade in from min to max in increments of 5 points:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
@@ -21,7 +21,7 @@ void loop() {
   // fade out from max to min in increments of 5 points:
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
     // sets the value (range from 0 to 255):
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }
