b590423ddb757a6a5882ab88d11926dbd5bd8f34
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-04-01 22:46:33 -0700

    Update level-meter.ino

--- level-meter.ino
+++ level-meter.ino
@@ -5,9 +5,9 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
 
   Serial.begin(9600);
 }
@@ -18,18 +18,18 @@ void loop() {
   sensorValue = analogRead(sensorPin);
 
   if (sensorValue == 1023) {
-    digitalWrite(13, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(11, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
     sensorValue = analogRead(sensorPin);
   } else if (sensorValue <= 255) {
-    digitalWrite(13, HIGH);//LED1 - 3/4
+    digitalWrite(5, HIGH);//LED1 - 3/4
     sensorValue = analogRead(sensorPin);
   } else if (sensorValue <= 510) {
-    digitalWrite(12, HIGH);//LED2 - 2/4
+    digitalWrite(6, HIGH);//LED2 - 2/4
     sensorValue = analogRead(sensorPin);
   } else if (sensorValue <= 766) {
-    digitalWrite(11, HIGH); //LED3 - 1/4
+    digitalWrite(9, HIGH); //LED3 - 1/4
     sensorValue = analogRead(sensorPin);
   }
   
