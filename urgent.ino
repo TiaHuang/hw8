f6d0a9488d662a0507e8046ed5f46175396000f6
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-04-01 22:49:19 -0700

    Update urgent.ino

--- urgent.ino
+++ urgent.ino
@@ -2,18 +2,18 @@ int sensorValue = 0;
 int sensorPin = A0;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
 
   Serial.begin(9600);
 }
 void loop() {
   for (int fadeValue = 0 ; fadeValue <= 200; fadeValue += 200) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(50);
   }
 
   for (int fadeValue = 200 ; fadeValue >= 0; fadeValue -= 200) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(50);
   }
 }
