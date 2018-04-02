64b24205645e646e6f3962fa7308662f170a4af0
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-04-01 22:49:38 -0700

    Update wrong-turn.ino

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -2,18 +2,18 @@ int sensorValue = 0;
 int sensorPin = A0;  
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
 
   Serial.begin(9600);
 }
 void loop() {
   for (int fadeValue = 0 ; fadeValue <= 800; fadeValue += 5) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(10);
   }
 
   for (int fadeValue = 800 ; fadeValue >= 0; fadeValue -= 5) {
-    analogWrite(13, fadeValue);
+    analogWrite(5, fadeValue);
     delay(10);
   }
 }
